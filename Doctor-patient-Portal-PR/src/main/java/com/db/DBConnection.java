913cbdf70f481efc735d989b96b6ec17046fa89f
adarsha Dalabehera <adarshadalabehera792@gmail.com>
2024-10-20 20:12:42 +0530

    add database by adarsha..

---
+++ Doctor-patient-Portal-PR/src/main/java/com/db/DBConnection.java
@@ -0,0 +1,24 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+	
+	private static Connection conn;
+	public static Connection getConn()
+	{
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/doctor_patient_portal","root","adarsha2005");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+}
--- Doctor-patient-Portal-PR/src/main/webapp/index.jsp
+++ Doctor-patient-Portal-PR/src/main/webapp/index.jsp
@@ -1,3 +1,5 @@
+<%@page import="com.db.DBConnection"%>
+<%@page import="java.sql.Connection"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -19,6 +21,11 @@
 </head>
 <body>
 <%@include file="component/navbar.jsp"%>
+
+
+<% Connection conn=DBConnection.getConn(); 
+out.println(conn);
+%>
 
 
 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
